df04048572440391cc4af8831050ae51ae26464f
GargPriyanshu1112 <gargpriyanshu1112@gmail.com>
2024-01-13 15:21:45 +0530

    Breakout env. exploration

---
+++ dqn_tf.py
@@ -0,0 +1,7 @@
+MAX_EXPERIENCES = 5_00_000  # total size of replay buffer
+MIN_EXPERIENCES = (
+    5_000  # size of replay buffer when we commence training the neural network
+)
+TARGET_UPDATED_PERIOD = 10_000  # ?
+IM_SIZE = 84
+K = 4  # number of actions
