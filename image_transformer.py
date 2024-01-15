971326c6283d348f593204668c6df48bf860a42b
GargPriyanshu1112 <gargpriyanshu1112@gmail.com>
2024-01-15 12:49:38 +0530

    ImageTransformer class implementation done.

--- image_transformer.py
+++ image_transformer.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 
 
 class ImageTransformer:
-    def transform(self, img, IMG_SIZE=84):
+    def transform(self, img, img_size=84):
         img = tf.image.rgb_to_grayscale(img)
         img = tf.image.crop_to_bounding_box(img, 35, 0, 160, 160)
         img = tf.image.resize(
             img,
-            size=(IMG_SIZE, IMG_SIZE),
+            size=(img_size, img_size),
             method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
         )
         img = tf.cast(img, dtype=tf.float32) / 255.0
         img = tf.squeeze(img)
-        assert img.shape == (IMG_SIZE, IMG_SIZE)
+        assert img.shape == (img_size, img_size)
         return img
