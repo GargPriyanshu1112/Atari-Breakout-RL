065dcb6975c7662da89990fc7a8be7d1151d0fd4
GargPriyanshu1112 <gargpriyanshu1112@gmail.com>
2024-01-17 10:51:13 +0530

    Added init function.

--- image_transformer.py
+++ image_transformer.py
@@ -2,15 +2,19 @@ import tensorflow as tf
 
 
 class ImageTransformer:
-    def transform(self, img, img_size=84):
+    def __init__(self, h=84, w=84):
+        self.h = h
+        self.w = w
+
+    def transform(self, img):
         img = tf.image.rgb_to_grayscale(img)
         img = tf.image.crop_to_bounding_box(img, 35, 0, 160, 160)
         img = tf.image.resize(
             img,
-            size=(img_size, img_size),
+            size=(self.h, self.w),
             method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
         )
         img = tf.cast(img, dtype=tf.float32) / 255.0
         img = tf.squeeze(img)
-        assert img.shape == (img_size, img_size)
+        assert img.shape == (self.h, self.w)
         return img
