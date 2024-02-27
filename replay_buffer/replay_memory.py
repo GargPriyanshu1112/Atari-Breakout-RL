d1efd1fbaf852531fd3333e94929431ef24e76e9
GargPriyanshu1112 <gargpriyanshu1112@gmail.com>
2024-02-27 12:51:18 +0530

    refactoring

--- replay_buffer/replay_memory.py
+++ replay_buffer/replay_memory.py
@@ -8,29 +8,28 @@ class ReplayMemory:
         buffer_size=500000,
         frame_h=84,
         frame_w=84,
-        batch_size=32,
         num_stacked_frames=4,
+        batch_size=32,
     ):
         self.buffer_size = buffer_size
-        self.h = frame_h
-        self.w = frame_w
         self.batch_size = batch_size
         self.num_stacked_frames = num_stacked_frames
-        self.buffer_pos = 0
-        self.count = 0
 
-        # Pre-allocate memory for replay buffer
+        self.count = 0
+        self.buffer_pos = 0  # tracks current position in the buffer
+
+        # Pre-allocate memory to store experience
         self.actions = np.empty(buffer_size, dtype=np.int32)
         self.rewards = np.empty(buffer_size, dtype=np.float32)
-        self.frames = np.empty((buffer_size, frame_h, frame_w), dtype=np.float32)
+        self.frames = np.empty((buffer_size, frame_h, frame_w), dtype=np.uint8)
         self.done_flags = np.empty(buffer_size, dtype=np.bool_)
 
-        # Pre-allocate memory for a minibatch
+        # Pre-allocate memory to store a minibatch we sample
         self.states = np.empty(
-            (batch_size, num_stacked_frames, frame_h, frame_w), dtype=np.float32
+            (batch_size, num_stacked_frames, frame_h, frame_w), dtype=np.uint8
         )
         self.next_states = np.empty(
-            (batch_size, num_stacked_frames, frame_h, frame_w), dtype=np.float32
+            (batch_size, num_stacked_frames, frame_h, frame_w), dtype=np.uint8
         )
         self.indices = np.empty(batch_size, dtype=np.int32)
 
@@ -44,6 +43,13 @@ class ReplayMemory:
         self.count = max(self.count, self.buffer_pos + 1)
         # Update current buffer position
         self.buffer_pos = (self.buffer_pos + 1) % self.buffer_size
+
+    def get_state(self, frame_idx):
+        if frame_idx < self.num_stacked_frames - 1:
+            raise ValueError("`frame_idx` cannot be less than 3.")
+        if self.count == 0:
+            raise ValueError("The replay memory is empty.")
+        return self.frames[frame_idx + 1 - self.num_stacked_frames : frame_idx + 1, ...]
 
     def get_valid_indices(self):
         """Helper function to sample a batch"""
@@ -61,20 +67,13 @@ class ReplayMemory:
                 break
             self.indices[i] = idx
 
-    def get_state(self, frame_idx):
-        if frame_idx < self.num_stacked_frames - 1:
-            raise ValueError("`frame_idx` cannot be less than 3.")
-        if self.count == 0:
-            raise ValueError("The replay memory is empty.")
-        return self.frames[frame_idx + 1 - self.num_stacked_frames : frame_idx + 1, ...]
-
     def get_batch(self):
         """Returns a batch of state transitions."""
         self.get_valid_indices()
 
-        for i, idx in enumerate(self.indices):
-            self.states[i] = self.get_state(idx - 1)  # ??
-            self.next_states[i] = self.get_state(idx)  # ??
+        for i, frame_idx in enumerate(self.indices):
+            self.states[i] = self.get_state(frame_idx - 1)  # ??
+            self.next_states[i] = self.get_state(frame_idx)  # ??
 
         return (
             np.transpose(self.states, axes=[0, 2, 3, 1]),
