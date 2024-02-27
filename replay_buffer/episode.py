3798f4393c24e5934ff86acf7db918818b58d554
GargPriyanshu1112 <gargpriyanshu1112@gmail.com>
2024-02-27 12:50:09 +0530

    bugfix: training will continue from the previous `eps` value.

--- replay_buffer/episode.py
+++ replay_buffer/episode.py
@@ -1,85 +1,81 @@
 import time
 import numpy as np
 
-TARGET_UPDATE_PERIOD = 10000  # steps after which we update the target model's weights
+
+# Returns next state by shifting each frame by 1. Removes the oldest frame from
+# the state and concatenates the latest frame to its other end.
+def get_next_state(state, frame):
+    new_state = np.append(state[:, :, 1:], np.expand_dims(frame, axis=-1), axis=-1)
+    assert new_state.ndim == 3  # new_state.shape == (H, W, NUM_STACKED_FRAMES)
+    return new_state
 
 
-def get_new_state(state, obs):
-    # print(np.append(state[:, :, 1:], np.expand_dims(obs, axis=-1), axis=-1).shape)
-    return np.append(state[:, :, 1:], np.expand_dims(obs, axis=-1), axis=-1)
-
-
-def learn(base_network, target_network, replay_memory, gamma):
+def learn(main_network, target_network, replay_memory, gamma):
+    # Get train batch
     states, actions, rewards, next_states, done_flags = replay_memory.get_batch()
     # print(
     #     states.shape, actions.shape, rewards.shape, next_states.shape, done_flags.shape
     # )
 
     # Get the target
-    pred_Qs = target_network.predict(next_states)
+    next_Qvals = target_network.predict(next_states)
     # print(f"pred_Qs shape: {pred_Qs.shape}")
-    maxQs = np.max(pred_Qs, axis=1)
+    nextQs = np.max(next_Qvals, axis=1)
     # print(f"maxQs shape: {maxQs.shape}")
-    targets = rewards + np.invert(done_flags).astype(np.float32) * gamma * maxQs
+    targets = rewards + np.invert(done_flags).astype(np.float32) * gamma * nextQs
     # print(f"targets shape: {targets.shape}")
-
-    loss = base_network.update(states, actions, targets)
+    loss = main_network.update(states, actions, targets)
     return loss
 
 
 def play_episode(
     env,
-    base_network,
+    main_network,
     target_network,
     img_transformer,
     replay_memory,
     step_count,
+    eps,
+    eps_min,
+    eps_change,
     num_stacked_frames=4,
     gamma=0.99,
-    eps=1.0,
-    eps_min=0.1,
+    target_update_period=10000,
 ):
     start_time = time.time()
 
     # Get initial episode state
     obs, _ = env.reset()
     frame = img_transformer.transform(obs)
-    state = np.stack([frame] * num_stacked_frames, axis=-1)  # 2
-    # print(state.shape)
+    state = np.stack([frame] * num_stacked_frames, axis=-1)
     assert state.ndim == 3
 
     done = False
     episode_reward = 0
     num_episode_steps = 0
-    eps_change = (eps - eps_min) / replay_memory.buffer_size
     while not done:
         # Update target network
-        if step_count % TARGET_UPDATE_PERIOD == 0:
-            print(f"Copying base model parameters to the target model...")
-            target_network.copy_weights(base_network.model)
+        if step_count % target_update_period == 0:
+            print(f"\nCopying base model parameters to the target model...\n")
+            target_network.copy_weights(main_network.model)
 
-        action = base_network.sample_action(state, eps)
+        action = main_network.sample_action(state, eps)
         obs, reward, terminated, truncated, _ = env.step(action)
         frame = img_transformer.transform(obs)
-        new_state = get_new_state(state, frame)  # how random will help?
-        assert new_state.shape == (
-            img_transformer.h,
-            img_transformer.w,
-            num_stacked_frames,
-        )
+        next_state = get_next_state(state, frame)  # how random will help?
         replay_memory.add_experience(action, frame, reward, terminated or truncated)
 
         # Train
-        loss = learn(base_network, target_network, replay_memory, gamma)
+        loss = learn(main_network, target_network, replay_memory, gamma)
 
-        end_time = time.time()
-        duration = end_time - start_time
-
-        state = new_state
+        state = next_state
+        eps = max(eps_min, eps - eps_change)
+        done = terminated or truncated
         episode_reward += reward
         num_episode_steps += 1
         step_count += 1
-        eps = max(eps_min, eps - eps_change)
-        done = terminated or truncated
 
-    return duration, loss, episode_reward, num_episode_steps, step_count
+    end_time = time.time()
+    duration = end_time - start_time
+
+    return duration, episode_reward, num_episode_steps, step_count, eps
