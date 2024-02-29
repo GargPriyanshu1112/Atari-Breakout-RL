93d69c36cc12cca96cfda5b3711655a3a5082567
GargPriyanshu1112 <gargpriyanshu1112@gmail.com>
2024-02-29 19:45:23 +0530

    Refactor: Merged ValueNetwork and PolicyNetwork class into one (ActorCritic).

--- A3C/networks.py
+++ A3C/networks.py
@@ -1,75 +1,41 @@
 import tensorflow as tf
-import tensorflow_probability as tfp
-from keras.models import Sequential, Model
+from keras.models import Model
 from keras.layers import Input, Rescaling, Conv2D, Flatten, Dense
 from keras.optimizers import RMSprop
 
 
-def get_shared_layers():
-    return Sequential(
-        [
-            Rescaling(scale=1.0 / 255),
-            Conv2D(16, kernel_size=(8, 8), strides=(4, 4), activation="relu"),
-            Conv2D(32, kernel_size=(4, 4), strides=(2, 2), activation="relu"),
-            Flatten(),
-            Dense(units=256, activation="relu"),
-        ]
-    )
-
-
-class ValueNetwork:
-    def __init__(self, inp_shape, shared_layers):
+class ActorCritic:
+    def __init__(self, inp_shape=(84, 84, 4), num_actions=4, reg_const=0.01):
         inputs = Input(shape=inp_shape)
-        x = shared_layers(inputs)
-        outputs = Dense(units=1)(x)
+        x = Rescaling(scale=1.0 / 255)(inputs)
+        x = Conv2D(16, kernel_size=(8, 8), strides=(4, 4), activation="relu")(x)
+        x = Conv2D(32, kernel_size=(4, 4), strides=(2, 2), activation="relu")(x)
+        x = Flatten()(x)
+        x = Dense(units=256)(x)
+        pi = Dense(units=num_actions)(x)
+        v = Dense(units=1)(x)
 
-        self.model = Model(inputs=inputs, outputs=outputs)
-        self.optimizer = RMSprop(0.00025, 0.99, 0.0, 1e-6)
-        self.optimizer.build(self.model.trainable_weights)
-
-    def predict(self, states):
-        return tf.squeeze(self.model(states))
-
-    def loss_fn(self, targets, preds):
-        losses = tf.math.squared_difference(targets, preds)
-        return tf.reduce_sum(losses)
-
-
-class PolicyNetwork:
-    def __init__(self, inp_shape, num_actions, shared_layers, reg_const=0.01):
-        inputs = Input(shape=inp_shape)
-        x = shared_layers(inputs)
-        outputs = Dense(units=num_actions)(x)
-
-        self.model = Model(inputs=inputs, outputs=outputs)
+        self.actor = Model(inputs=inputs, outputs=pi)
+        self.critic = Model(inputs=inputs, outputs=v)
         self.reg_const = reg_const  # regularization constant
-        self.optimizer = RMSprop(0.00025, 0.99, 0.0, 1e-6)
-        self.optimizer.build(self.model.trainable_weights)
 
-    def get_logits(self, states):
-        return self.model(states)
+    def get_action_probs(self, states):
+        """Returns p(a| s)"""
+        return tf.nn.softmax(self.actor(states))
 
-    def get_probs(self, states):
-        return tf.nn.softmax(self.get_logits(states))
-
-    def sample_action(self, state):
-        logits = self.get_logits(state)
-        distibution = tfp.distributions.Categorical(logits=logits)  # READ
-        action = distibution.sample()[0]
-        return action
-
-    def loss_fn(self, action_probs, selected_action_probs, advantages):
+    def actor_loss_fn(self, action_probs, chosen_action_probs, advantages):
         # Policy loss
-        Lp = -(advantages * tf.math.log(selected_action_probs))
+        Lp = -(advantages * tf.math.log(chosen_action_probs))
         # Entropy
         H = -tf.reduce_sum(action_probs * tf.math.log(action_probs), axis=1)
         # Regularize the policy loss (encourages exploration)
         loss = tf.reduce_sum(Lp + self.reg_const * H)
         return loss
 
+    def get_v_estimate(self, states):
+        """Return V(s)"""
+        return tf.squeeze(self.critic(states))
 
-def get_networks(inp_shape, num_actions):
-    shared_layers = get_shared_layers()
-    value_network = ValueNetwork(inp_shape, shared_layers)
-    policy_network = PolicyNetwork(inp_shape, num_actions, shared_layers)
-    return value_network, policy_network
+    def critic_loss_fn(self, targets, preds):
+        losses = tf.math.squared_difference(targets, preds)
+        return tf.reduce_sum(losses)
